5f0008e25ae2283d4223d0755ae8cd966823e071
PKgothatso <ksp331@outlook.com>
2023-10-14 11:49:53 -0700

    Prints all possible combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	nt n;
+	int n;
 
 	for (n = 48; n < 58; n++)
 	{
