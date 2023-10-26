b405bc0d836e1d9dc11375cd402e80b7477fb43e
PKgothatso <ksp331@outlook.com>
2023-10-26 11:31:25 -0700

    function that returns the value of x raised to the power of y

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * wildcmp - Compares two strings considering wildcard characters.
+ * @s1: The first string to compare.
+ * @s2: The second string, which may contain wildcard characters.
+ *
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+	if (*s2 != '\0' && *s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+	return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+	return (wildcmp(s1 + 1, s2 + 1));
+	}
+}
