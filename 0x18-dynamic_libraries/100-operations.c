b1848c8a9c8147c6a0426ab498057d474f62138c
PKgothatso <ksp331@outlook.com>
2024-01-20 07:58:56 -0800

    Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: first int.
+ * @b: Second int.
+ * Return: Final answer.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtract two integers.
+ * @a: First int.
+ * @b: Second int.
+ * Return: Final answer.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiply two integers.
+ * @a: First int.
+ * @b: Second int.
+ * Return: Answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divide two integers.
+ * @a: First int.
+ * @b: Second int.
+ * Return: Final answer.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Divides  two integers.
+ * @a: First int.
+ * @b: Second int.
+ * Return: Final answer.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
